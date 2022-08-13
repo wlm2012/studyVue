2174d11a55277421b2c992e8ff289d817fe72fd4
wlm2012 <541253785@qq.com>
2022-08-13 09:41:55 +0800

    增加ant design 增加组件按钮

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+    <a-button type="primary" :size="size">Primary</a-button>
+    <a-button :size="size">Normal</a-button>
+    <a-button type="dashed" :size="size">Dashed</a-button>
+    <a-button danger :size="size">Danger</a-button>
+    <a-button type="link" :size="size">Link</a-button>
+    <br />
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
