30921681f2041e10515d851105f9a7f7534ddfdf
wlm2012 <541253785@qq.com>
2022-08-14 14:48:07 +0800

    组件 theHeader

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo"/>
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
     <a-layout-footer style="text-align: center">
       ebook1
@@ -20,6 +8,18 @@
   </a-layout>
 </template>
 
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import theHeader from '@/components/the-header.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    theHeader,
+  },
+});
+</script>
 
 
 <style>
