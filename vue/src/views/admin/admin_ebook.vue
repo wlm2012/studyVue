cf34b6d153647bdae16da101c3796d47a63911f4
wlm2012 <541253785@qq.com>
2022-08-23 20:35:45 +0800

    修改电子书管理页面

--- vue/src/views/admin/admin_ebook.vue
+++ vue/src/views/admin/admin_ebook.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>This is an ebook page</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
